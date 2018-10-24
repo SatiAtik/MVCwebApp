f4cc81ffede22532ea972a5a2c9a9722a5132d9d
satiAtik <stkeskin@gmail.com>
2018-10-24 23:28:58 +0300

    Update

--- MVCwebApp/App_Start/FilterConfig.cs
+++ MVCwebApp/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using MVCwebApp.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MVCwebApp
@@ -7,7 +8,8 @@ namespace MVCwebApp
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new EmployeeExceptionFilter());
         }
     }
 }
---
+++ MVCwebApp/Logger/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MVCwebApp.Logger
+{
+    public class FileLogger
+    {
+        public void LogException(Exception e)
+        {
+            File.WriteAllLines("C://MVCwebAppExample//Error//" + DateTime.Now.ToString("dd-MM-yyyy mm hh ss") + ".txt",
+                new string[]
+                {
+                   "Message:"+e.Message,
+                   "StackTrace:"+e.StackTrace
+                });
+        }
+    }
+}
